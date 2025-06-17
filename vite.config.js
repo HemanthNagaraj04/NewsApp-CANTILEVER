52de1d0069d574d488115e49e24e49cf731aab5a
HemanthNagaraj04 <hemanthnagaraj2004@gmail.com>
2025-06-17 11:43:31 +0530

    added deploy base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/NewsApp-CANTILEVER/',
   plugins: [react(),
     tailwindcss(),
   ],
